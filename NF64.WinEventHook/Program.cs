7d941c18cbf21fd137ecf50f2eb5be7220db36ca
NF64-NFc <n.fushimi@nfc64.net>
2021-07-14 23:09:05 +0900

    *

---
+++ NF64.WinEventHook/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NF64.WinEventHook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
